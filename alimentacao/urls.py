8ad64ae794401d5881fc94b25117e46af20cc231
EllisCarvalho3 <146475582+EllisCarvalho3@users.noreply.github.com>
2026-01-31 15:08:37 -0300

    .

--- alimentacao/urls.py
+++ alimentacao/urls.py
@@ -1,13 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
-
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('historico/', views.historico, name='historico'),
-      path('cadastro/', views.cadastrar, name='cadastro'),
-      path("refeicoes/", include("refeicoes.urls")),
-      
+    path("", views.home, name="home"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("historico/", views.historico, name="historico"),
+    path("cadastro/", views.cadastrar, name="cadastro"),
 
+    # rotas do app refeições
+    path("refeicoes/", include("refeicoes.urls")),
 ]
