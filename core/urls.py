1947ad8d8b5ee9191550fed4ba0005aaaf80ea05
EllisCarvalho3 <146475582+EllisCarvalho3@users.noreply.github.com>
2026-02-03 11:28:26 -0300

    .

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('alimentacao.urls')),
-   path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path("perfil/", include("perfil.urls")),
     path("refeicoes/", include("refeicoes.urls")),
 ]
