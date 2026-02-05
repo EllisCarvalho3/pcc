8db7ed846bb393a0f7489d705537f2d01ce74a4c
EllisCarvalho3 <146475582+EllisCarvalho3@users.noreply.github.com>
2026-02-04 21:26:03 -0300

    .

--- perfil/forms.py
+++ perfil/forms.py
@@ -5,10 +5,23 @@ class PerfilForm(forms.ModelForm):
     class Meta:
         model = Perfil
         fields = [
-    'peso',
-    'altura',
-    'idade',
-    'atividade',
-    'objetivo'
-]
+            'peso',
+            'altura',
+            'idade',
+            'atividade',
+            'objetivo'
+        ]
 
+    def clean(self):
+        cleaned_data = super().clean()
+
+        atividade = cleaned_data.get("atividade")
+        objetivo = cleaned_data.get("objetivo")
+
+        if not atividade:
+            self.add_error("atividade", "Selecione o nível de atividade.")
+
+        if not objetivo:
+            self.add_error("objetivo", "Selecione um objetivo.")
+
+        return cleaned_data
