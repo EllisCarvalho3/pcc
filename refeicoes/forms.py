8a782e0dfa5fb36cafe89701f93aca81f5bc282a
EllisCarvalho3 <146475582+EllisCarvalho3@users.noreply.github.com>
2026-02-04 22:35:14 -0300

    .

--- refeicoes/forms.py
+++ refeicoes/forms.py
@@ -5,9 +5,27 @@ class RefeicaoForm(forms.ModelForm):
     class Meta:
         model = Refeicao
         fields = [
-            'nome',
-            'carboidratos',
-            'proteinas',
-            'gorduras',
-            'porcao'
+            "nome",
+            "porcao",
+            "carboidratos",
+            "proteinas",
+            "gorduras",
         ]
+
+        widgets = {
+            "nome": forms.TextInput(attrs={
+                "placeholder": "Ex: Feijão cozido"
+            }),
+            "porcao": forms.NumberInput(attrs={
+                "placeholder": "Quantidade consumida (g)"
+            }),
+            "carboidratos": forms.NumberInput(attrs={
+                "placeholder": "Carboidratos por 100g"
+            }),
+            "proteinas": forms.NumberInput(attrs={
+                "placeholder": "Proteínas por 100g"
+            }),
+            "gorduras": forms.NumberInput(attrs={
+                "placeholder": "Gorduras por 100g"
+            }),
+        }
--- refeicoes/models.py
+++ refeicoes/models.py
@@ -5,20 +5,22 @@ class Refeicao(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
 
-    # valores por 100g
+    porcao = models.FloatField(help_text="Quantidade consumida em gramas")
+
     carboidratos = models.FloatField(help_text="Carboidratos por 100g")
     proteinas = models.FloatField(help_text="Proteínas por 100g")
     gorduras = models.FloatField(help_text="Gorduras por 100g")
-
-    porcao = models.FloatField(help_text="Porção consumida em gramas")
 
     data = models.DateField(auto_now_add=True)
 
     def calorias(self):
         fator = self.porcao / 100
-        carbo = self.carboidratos * fator
-        prot = self.proteinas * fator
-        gord = self.gorduras * fator
-        return carbo * 4 + prot * 4 + gord * 9
+        return (
+            (self.carboidratos * 4 +
+             self.proteinas * 4 +
+             self.gorduras * 9)
+            * fator
+        )
+
 
 
