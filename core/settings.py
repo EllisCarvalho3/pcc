cc240b51c10efb8d7c26fcb71fa1c8501c6944fa
EllisCarvalho3 <146475582+EllisCarvalho3@users.noreply.github.com>
2026-07-23 15:05:29 -0300

    .

--- core/settings.py
+++ core/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processors.static_version',
             ],
         },
     },
@@ -118,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'alimentacao' / 'static']
+STATIC_VERSION = '2'
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'home'
